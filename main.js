b5171825895755535dfd584a9d7ccaa0ea97f39a
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-11 23:32:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+function checkUsernames(currentUsers, newUsers) {
+    for (const newUser of newUsers) {
+        if (currentUsers.includes(newUser)) {
+            console.log(`Username "${newUser}" is unavailable. Please enter a new username.`);
+        }
+        else {
+            console.log(`Username "${newUser}" is available.`);
+        }
+    }
+}
+const currentUsers = ["John", "Admin", "Jack", "Ana", "Natalie"];
+const newUsers = ["Pablo", "Donald", "Calvin", "Natalie", "Emma"];
+checkUsernames(currentUsers, newUsers);
